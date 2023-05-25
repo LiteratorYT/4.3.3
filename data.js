761648e484805ab5625942f0105ec40ac79545e0
Ivan Akimov <litirik.yt@gmail.com>
2023-05-25 16:08:41 +0300

    dgsdgdf

---
+++ data.js
@@ -0,0 +1,53 @@
+export const context = {
+    array: ['a', 'b', 'c'],
+  };
+  export const context = {
+    languages: [
+      {
+        name: 'HTML',
+      },
+      {
+        name: 'CSS',
+      },
+      {
+        name: 'JavaScript',
+      },
+    ],
+  };
+  export const context = {
+    languages: [
+      {
+        name: 'HTML',
+        modern: true,
+      },
+      {
+        name: 'CSS',
+        modern: true,
+      },
+      {
+        name: 'JavaScript',
+        modern: true,
+      },
+    ],
+  };
+  export const context = {
+    languages: [
+      {
+        name: 'HTML',
+        modern: true,
+      },
+      {
+        name: 'CSS',
+        modern: true,
+      },
+      {
+        name: 'JavaScript',
+        modern: true,
+      },
+      {
+        name: 'COBOL',
+        modern: false,
+      },
+    ],
+  };
+  
